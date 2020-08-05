c2f6a5d40b12e6af168339a85e1293b854cf638a
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-05 22:07:42 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,8 @@
 def my_each(collection)
   i = 0
-  while i < (collection[i])
-  i = i + 1
-end
-collection
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
